35e013b2fe65be2dc1a3f122d6866fad8bbfeb44
Annabel00 <jerrea@student.chalmers.se>
2020-12-22 17:15:39 +0100

    Create testkod.py

---
+++ testkod.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("här är en kod som jag hoppas kunna få upp till Github")
+print("och jag kunde öppna den :))))))/ Annabel")
+print("yayyyyy")
+
+
+
+
