eba08fb816ef64eeb728f884f5b9c474a1e09928
Annabel00 <jerrea@student.chalmers.se>
2021-01-19 15:43:08 +0100

    Egen modell
    Egen modell som har med beteendeförändringar och att immuniteten försvinner men ändå inte får en andra våg

--- SIR_model-Copy1.py
+++ SIR_model-Copy1.py
@@ -11,36 +11,37 @@ import matplotlib.pyplot as plt
 def deriv(y, t, N, beta, gamma, delta, theta, my):
     S, E, I, R ,B = y
     dBdt = my * I / N
-    dSdt = -beta * (1-B) * S * I / N + theta * R# S(t) – susceptible (de som är mottagliga för infektion).
-    #dEdt = beta *(1-B) * S * I / N - gamma * E #Exposed (har blivit utsatta för sjukdomen)
+    dSdt = -beta * (1-B) * S * I / N + theta * R # S(t) – susceptible (de som är mottagliga för infektion).
+    dEdt = beta *(1-B) * S * I / N - gamma * E #Exposed (har blivit utsatta för sjukdomen)
     dIdt = delta * E - gamma * I # I(t) – infected (de som har pågående infektion)
     dRdt = gamma * I - theta * R #Removed (tillfrisknar)
-    return dSdt, dIdt, dRdt, dBdt
+    if dIdt >= 10:
+        my = 2
+    return dSdt, dEdt, dIdt, dRdt, dBdt
 
 
 
 
 # describe the parameters
-N =  100000 #Totala befolkningen N=s(t)+I(t)+R(t)
+N =  10000000 #Totala befolkningen N=s(t)+I(t)+R(t)
 D = 7 #Hur länge infektionen varar
-gamma = 1.0 / D #Removal (hur många som tillfrisknar per dag)
-delta = 1.0 / 5.0 #incubation period of five days
+gamma = 2 / D #Removal (hur många som tillfrisknar per dag)
+delta = 1.0 / 7.0 #incubation period of five days
 theta = 0.01 / D #tidigarer immuna som insjuknar igen
-my = 0.3
+my = 0.2
 R_0 = 2.5 #Reproduktionstalet  
 beta = R_0 * gamma #r_0=beta/gamma. antal som smittas per infekterad och per tid (beror på virusets egenskaper samt hur vi beter oss).  
-S0,I0, R0, B0 = N-1, 1, 0, 0, 0  # initial conditions: one infected, rest susceptible
- 
+S0, E0, I0, R0, B0 = N-1, 1, 0, 0, 0  # initial conditions: one infected, rest susceptible
 #Rt = R0 * S(t)/Ntot* (1 – b). b = effekt av policy och beteendeförändringar
 
 
 
 t = np.linspace(0, 999, 1000) # Grid of time points (in days)
-y0 = S0,I0, R0, B0 # Initial conditions vector
+y0 = S0, E0, I0, R0, B0 # Initial conditions vector
  
 # Integrate the SIR equations over the time grid, t.
 ret = odeint(deriv, y0, t, args=(N, beta, gamma, delta,theta,my))
-S, I, R, B = ret.T
+S, E, I, R, B = ret.T
 
 
 
